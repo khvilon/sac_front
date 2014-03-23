a2368491048a2341d500a1925ca4771be9c60bc2
Andrey Novikov <anovikov@at-consulting.ru>
2014-03-23 15:37:30 +0400

    Use current host as API host

--- compile/js/config.js
+++ compile/js/config.js
@@ -5,7 +5,7 @@ var ConfigApp = {
 		"MAP": "/static/images/map/",
 		"MINI-MAP": "/static/images/mini/"
 	},
-	"API-HOST": "http://192.168.1.110:3000",
+	"API-HOST": location.origin,
 	"FOOTER-NAV": {
 		"MAP": {
 			"title": "округа"
